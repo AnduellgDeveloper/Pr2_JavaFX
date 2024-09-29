f3e233ad4508076c1d70797891fbd473b16f5407
AnduellgDeveloper <anduellgames@gmail.com>
2024-09-28 23:00:31 -0500

    Cambios

--- co.edu.uniquindio.MarketPlace_Fx/src/main/java/co/edu/uniquindio/marketplace_fx/marketplace_app/MarketPlaceApplication.java
+++ co.edu.uniquindio.MarketPlace_Fx/src/main/java/co/edu/uniquindio/marketplace_fx/marketplace_app/MarketPlaceApplication.java
@@ -10,15 +10,15 @@ import java.io.IOException;
 public class MarketPlaceApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MarketPlaceApplication.class.getResource("Login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        FXMLLoader fxmlLoader = new FXMLLoader(MarketPlaceApplication.class.getResource("Seller-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("MarketPlace - Login");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[]args) {
         launch();
     }
 }
