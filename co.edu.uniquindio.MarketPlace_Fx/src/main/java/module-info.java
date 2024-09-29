106d9e8f98c93dab766cd8f2d5036a3dc69f0877
AnduellgDeveloper <anduellgames@gmail.com>
2024-09-28 22:18:34 -0500

    Cambioos

--- co.edu.uniquindio.MarketPlace_Fx/src/main/java/module-info.java
+++ co.edu.uniquindio.MarketPlace_Fx/src/main/java/module-info.java
@@ -5,4 +5,6 @@ module co.edu.uniquindio.marketplace_fx.marketplace_app {
 
     opens co.edu.uniquindio.marketplace_fx.marketplace_app to javafx.fxml;
     exports co.edu.uniquindio.marketplace_fx.marketplace_app;
+    opens co.edu.uniquindio.marketplace_fx.marketplace_app.controller;
+    exports co.edu.uniquindio.marketplace_fx.marketplace_app.controller;
 }
