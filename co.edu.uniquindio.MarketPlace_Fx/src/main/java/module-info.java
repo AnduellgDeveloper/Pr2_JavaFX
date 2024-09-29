0ad2d890cfbf1da19c4b9068a17a49e11791e01c
AnduellgDeveloper <151770378+AnduellgDeveloper@users.noreply.github.com>
2024-09-28 19:11:55 -0500

    Se crea proyecto JavaFx

---
+++ co.edu.uniquindio.MarketPlace_Fx/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.marketplace_fx.marketplace_app {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.marketplace_fx.marketplace_app to javafx.fxml;
+    exports co.edu.uniquindio.marketplace_fx.marketplace_app;
+}
