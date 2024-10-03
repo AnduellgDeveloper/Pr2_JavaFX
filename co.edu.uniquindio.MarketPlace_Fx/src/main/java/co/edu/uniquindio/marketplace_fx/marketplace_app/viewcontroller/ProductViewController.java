21857c3029d1fc3b33b3fd03a71515b05b95ef26
AnduellgDeveloper <anduellgames@gmail.com>
2024-10-03 11:28:54 -0500

    Cambioos

--- co.edu.uniquindio.MarketPlace_Fx/src/main/java/co/edu/uniquindio/marketplace_fx/marketplace_app/viewcontroller/ProductViewController.java
+++ co.edu.uniquindio.MarketPlace_Fx/src/main/java/co/edu/uniquindio/marketplace_fx/marketplace_app/viewcontroller/ProductViewController.java
@@ -2,7 +2,11 @@ package co.edu.uniquindio.marketplace_fx.marketplace_app.viewcontroller;
 
 import co.edu.uniquindio.marketplace_fx.marketplace_app.controller.ProductController;
 import co.edu.uniquindio.marketplace_fx.marketplace_app.mapping.dto.ProductDto;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -12,7 +16,15 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ProductViewController {
+
+    ProductController productController;
+    ObservableList<ProductDto> listProducts = FXCollections.observableArrayList();
+    ProductDto selectProduct;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm");
 
     @FXML
     private Button btnAddProduct;
@@ -36,19 +48,19 @@ public class ProductViewController {
     private TableView<ProductDto> tbProducts;
 
     @FXML
-    private TableColumn<ProductDto,String> tcCategory;
+    private TableColumn<ProductDto, String> tcCategory;
 
     @FXML
-    private TableColumn<ProductDto,String> tcEstatus;
+    private TableColumn<ProductDto, String> tcEstatus;
 
     @FXML
-    private TableColumn<ProductDto,String> tcName;
+    private TableColumn<ProductDto, String> tcName;
 
     @FXML
-    private TableColumn<ProductDto,String> tcPrice;
+    private TableColumn<ProductDto, Integer> tcPrice;  // Ahora de tipo Integer
 
     @FXML
-    private TableColumn<ProductDto,String> tcPublication;
+    private TableColumn<ProductDto, String> tcPublicationDate;  // Declaramos la columna para la fecha
 
     @FXML
     private TextField txtCategory;
@@ -64,77 +76,61 @@ public class ProductViewController {
 
     @FXML
     private ImageView viewProduct;
-
     @FXML
     void onAddProduct(ActionEvent event) {
-
     }
-
     @FXML
     void onRemoveProduct(ActionEvent event) {
-
     }
-
     @FXML
     void onStatusCancelled(ActionEvent event) {
-
     }
-
     @FXML
     void onStatusPublished(ActionEvent event) {
-
     }
-
     @FXML
     void onStatusSold(ActionEvent event) {
-
     }
-
     @FXML
     void onUpdateProduct(ActionEvent event) {
-
     }
     @FXML
     void initialize() {
         productController = new ProductController();
         initView();
     }
-
     private void initView() {
         initDataBinding();
         getProducts();
         tbProducts.getItems().clear();
-        tbProducts.setItems(listaClientes);
+        tbProducts.setItems(listProducts);
         listenerSelection();
     }
-
     private void getProducts() {
         listProducts.addAll(productController.getProducts());
     }
-
     private void initDataBinding() {
-        tcName.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().nombre()));
-        tc.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().apellido()));
-        tcCedula.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().cedula()));
-        tcEmail.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().email()));
-        tcDireccion.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().direccion()));
-    }
-
-    private void listenerSelection() {
-        tableCliente.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
-            clienteSeleccionado = newSelection;
-            mostrarInformacionCliente(clienteSeleccionado);
+        tcName.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().name()));
+        tcCategory.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().category()));
+        tcEstatus.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().status()));
+        tcPrice.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().price()));
+        tcPublicationDate.setCellValueFactory(cellData -> {
+            LocalDateTime publicationDate = cellData.getValue().publicationDate();
+            String formattedDate = publicationDate.format(formatter);
+            return new SimpleStringProperty(formattedDate);
         });
     }
-
-    private void mostrarInformacionCliente(ClienteDto clienteSeleccionado) {
-        if(clienteSeleccionado != null){
-            txtNombre.setText(clienteSeleccionado.nombre());
-            txtApellido.setText(clienteSeleccionado.apellido());
-            txtCedula.setText(clienteSeleccionado.cedula());
-            txtEmail.setText(clienteSeleccionado.email());
-            txtdireccion.setText(clienteSeleccionado.direccion());
+    private void listenerSelection() {
+        tbProducts.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            selectProduct = newSelection;
+            showProductInformation(selectProduct);
+        });
+    }
+    private void showProductInformation(ProductDto selectProduct) {
+        if(selectProduct != null){
+            txtName.setText(selectProduct.name());
+            txtCategory.setText(selectProduct.category());
+            txtImage.setText(selectProduct.image());
         }
     }
-
 }
