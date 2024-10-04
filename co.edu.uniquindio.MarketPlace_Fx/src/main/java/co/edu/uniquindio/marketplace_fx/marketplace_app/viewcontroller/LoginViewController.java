402a88f94ae4b48473dffb6d0001e922374fa453
nicolmarin <nicolmoreno090905@gmail.com>
2024-10-04 12:00:27 -0500

    cambios

--- co.edu.uniquindio.MarketPlace_Fx/src/main/java/co/edu/uniquindio/marketplace_fx/marketplace_app/viewcontroller/LoginViewController.java
+++ co.edu.uniquindio.MarketPlace_Fx/src/main/java/co/edu/uniquindio/marketplace_fx/marketplace_app/viewcontroller/LoginViewController.java
@@ -1,14 +1,20 @@
 package co.edu.uniquindio.marketplace_fx.marketplace_app.viewcontroller;
 
-
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-import java.awt.event.ActionEvent;
-
 public class LoginViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private PasswordField textPassword;
@@ -28,9 +34,27 @@ public class LoginViewController {
     }
 
     @FXML
+    void 3d123f(ActionEvent event) {
+
+    }
+
+    @FXML
     void 930a9a(ActionEvent event) {
 
     }
 
+    @FXML
+    void 3d123f(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert textPassword != null : "fx:id=\"textPassword\" was not injected: check your FXML file 'Login-view.fxml'.";
+        assert buttonLogin != null : "fx:id=\"buttonLogin\" was not injected: check your FXML file 'Login-view.fxml'.";
+        assert textUser != null : "fx:id=\"textUser\" was not injected: check your FXML file 'Login-view.fxml'.";
+        assert buttonRegister != null : "fx:id=\"buttonRegister\" was not injected: check your FXML file 'Login-view.fxml'.";
+
+    }
 }
 
